f0bd5bfed549d19e9752dd1c9a9601f47ca10d46
寺井 大樹 <teraidaiki@terai-no-MacBook-Air.local>
2016-05-22 02:48:26 +0900

    AWS SDKを2.4.2にバージョンアップ

--- Mobile/iOS/Pods/AWSCognitoIdentityProvider/AWSCognitoIdentityProvider/Internal/AWSCognitoIdentityProviderSrpHelper.m
+++ Mobile/iOS/Pods/AWSCognitoIdentityProvider/AWSCognitoIdentityProvider/Internal/AWSCognitoIdentityProviderSrpHelper.m
@@ -209,10 +209,10 @@ AWSJKBigInteger* finalizeSignedBigIntHash(CC_SHA256_CTX *ctx);
 
     CCHmacContext ctx;
     CCHmacInit(&ctx, kCCHmacAlgSHA256, self.authenticationKey.bytes, self.authenticationKey.length);
-    CCHmacUpdate(&ctx, self.serverState.poolName.UTF8String, self.serverState.poolName.length);
-    CCHmacUpdate(&ctx, self.clientState.userName.UTF8String, self.clientState.userName.length);
+    CCHmacUpdate(&ctx, self.serverState.poolName.UTF8String, (CC_LONG)[self.serverState.poolName lengthOfBytesUsingEncoding:NSUTF8StringEncoding]);
+    CCHmacUpdate(&ctx, self.clientState.userName.UTF8String, (CC_LONG)[self.clientState.userName lengthOfBytesUsingEncoding:NSUTF8StringEncoding]);
     CCHmacUpdate(&ctx, self.serverState.serviceSecretBlock.bytes, self.serverState.serviceSecretBlock.length);
-    CCHmacUpdate(&ctx, dateStr.UTF8String, dateStr.length);
+    CCHmacUpdate(&ctx, dateStr.UTF8String, (CC_LONG)[dateStr lengthOfBytesUsingEncoding:NSUTF8StringEncoding]);
     CCHmacFinal(&ctx, hashOutput.mutableBytes);
 
     return [NSData dataWithData:hashOutput];
@@ -263,10 +263,8 @@ AWSJKBigInteger* finalizeSignedBigIntHash(CC_SHA256_CTX *ctx);
     NSTimeZone *timeZone = [NSTimeZone timeZoneWithName:@"UTC"];
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     dateFormatter.timeZone = timeZone;
-    // SDKのバグ、USロケールでないとエラーになる
-    // https://forums.aws.amazon.com/thread.jspa?messageID=719881
-    dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
     dateFormatter.dateFormat = @"EEE MMM d HH:mm:ss 'UTC' yyyy";
+    dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
     return [dateFormatter stringFromDate:date];
 }
 
